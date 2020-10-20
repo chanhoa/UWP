243860c38875b5f519455d88c7925ef92dc23c97
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2020-10-20 08:16:51 +0700

    chanhoa

---
+++ Lap06/U40-Example1/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40_Example1.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var Books = new List<Book>();
+            Books.Add(new Book { BookId = 1, Title = "Doremon", Author = "Fujio", CoverImage = "Assets/1.jpg" });
+
+            Books.Add(new Book { BookId = 2, Title = "PoKeMon", Author = "Takeshi Shudo", CoverImage = "Assets/2.jpg" });
+
+            Books.Add(new Book { BookId = 3, Title = "Dragon Ball", Author = "Toriyama Akira", CoverImage = "Assets/3.jpg" });
+
+            return Books;
+        }
+    }
+}
